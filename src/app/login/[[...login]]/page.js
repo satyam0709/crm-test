4c7bc48d62a3d0f9753c2f90131871339103f6bb
Satyam Kumar <iamsatyamsingh91@gmail.com>
2026-04-01 16:56:59 +0530

    Cleanup: remove duplicate root Next.js files, keep frontend only

--- src/app/login/[[...login]]/page.js
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function LoginPage() {
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '100px' }}>
-      <SignIn routing="path" path="/login" />
-    </div>
-  );
-}

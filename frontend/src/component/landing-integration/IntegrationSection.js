0b44d0ff4ae8e0572e10a3b1aa23271e510ff689
Satyam Kumar <iamsatyamsingh91@gmail.com>
2026-04-02 11:46:37 +0530

    inte updated

--- frontend/src/component/landing-integration/IntegrationSection.js
+++ frontend/src/component/landing-integration/IntegrationSection.js
@@ -23,7 +23,7 @@ export default function IntegrationsSection() {
             <Link href="/get-started" className={styles.btnPrimary}>
               Get Started
             </Link>
-            <Link href="/connect-us" className={styles.btnOutline}>
+            <Link href="/contact-us" className={styles.btnOutline}>
               Connect with Us
             </Link>
           </div>
